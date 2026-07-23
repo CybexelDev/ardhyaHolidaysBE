27ccc198efcb0e0b15ba8aae6e4321ddddc0a036
N-meapp <developersnme@gmail.com>
2026-07-23 15:50:57 +0530

    ashi

--- ardhyaholidaysBE/routes/admin.js
+++ ardhyaholidaysBE/routes/admin.js
@@ -2,15 +2,17 @@ const express = require('express')
 const router = express.Router()
 const uploadsMulter = require('../config/cloudinary');
 const { addVehicleData, deleteVehicleData, updateVehicleData, addPackageData, deletePackageData,  addCategory, updatePackageData, vehicleBooking, addTestimonial, deleteTestimonial, deleteCategory, updateCategory} = require('../Controllers/adminController')
-const { getVahicleData, getCategory,  getPackageData} = require('../Controllers/userController')
+const { getVahicleData, getCategory,  getPackageData, getTestimonials} = require('../Controllers/userController')
 
 router.post('/addVehicleData', uploadsMulter, addVehicleData);
 router.delete('/deleteVehicle/:id', deleteVehicleData);
 router.put('/updateVehicle/:id', uploadsMulter, updateVehicleData);
+router.get('/getVahicleData', getVahicleData)
 
 router.post('/addPackage', uploadsMulter, addPackageData);
 router.delete('/deletePackage/:id', deletePackageData);
 router.put('/updatePackage/:id', uploadsMulter, updatePackageData);
+router.get('/getPackageData', getPackageData)
 
 router.post('/addCategory', addCategory);
 router.get('/getCategory', getCategory)
@@ -23,6 +25,7 @@ router.get('/getPackageData', getPackageData)
 
 router.post('/addTestimonial', uploadsMulter, addTestimonial);
 router.delete('/deleteTestimonial/:id', deleteTestimonial);
+router.get('/getTestimonials', getTestimonials)
 
 
 module.exports = router;
